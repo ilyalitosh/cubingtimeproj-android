2c30509a93dc7fc986ef9d9a454de7a2f7b561ac
ilya_litosh <ilya_litosh@mail.ru>
2018-09-30 23:05:39 +0300

    Добавлено PopupMenu в активности чата

---
+++ app/src/main/java/com/litosh/ilya/cubingtimeproj/chatactivity/ui/MoreFeaturesChatItemMenu.java
@@ -0,0 +1,45 @@
+package com.litosh.ilya.cubingtimeproj.chatactivity.ui;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.PopupMenu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.litosh.ilya.cubingtimeproj.R;
+
+public class MoreFeaturesChatItemMenu extends PopupMenu implements PopupMenu.OnMenuItemClickListener {
+
+    private Context mContext;
+
+    public MoreFeaturesChatItemMenu(@NonNull Context context, @NonNull View anchor) {
+        super(context, anchor);
+        mContext = context;
+        inflate(R.menu.menu_more_features_chat_item);
+        setOnMenuItemClickListener(this);
+    }
+
+    public MoreFeaturesChatItemMenu(@NonNull Context context, @NonNull View anchor, int gravity) {
+        super(context, anchor, gravity);
+        mContext = context;
+    }
+
+    public MoreFeaturesChatItemMenu(@NonNull Context context, @NonNull View anchor, int gravity, int popupStyleAttr, int popupStyleRes) {
+        super(context, anchor, gravity, popupStyleAttr, popupStyleRes);
+        mContext = context;
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_more_features_chat_item_update:
+                Toast.makeText(mContext, "Скоро...", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.menu_more_features_chat_item_delete:
+                Toast.makeText(mContext, "Скоро...", Toast.LENGTH_SHORT).show();
+                return true;
+        }
+        return false;
+    }
+}
